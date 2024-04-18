872802392ad6e1bdbe801bdf4bb78e856ab121f3
Diego Maia <ovrdoz@gmail.com>
2024-04-18 01:01:03 -0300

    removed watchdog

--- app.py
+++ app.py
@@ -3,19 +3,11 @@ from flask_cors import CORS
 import os
 import json
 import re
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
 app = Flask(__name__)
 CORS(app)
 
 DATA_DIR = './data'
-
-class ChangeHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if event.src_path.endswith('.mok'):
-            print(f"Detected change in file: {event.src_path}")
-            load_routes()
 
 def convert_route_to_flask(route):
     return re.sub(r'\{([^}]+)\}', r'<\1>', route)
@@ -44,14 +36,8 @@ def register_route(route, method, content, status_code):
     app.add_url_rule(route, endpoint, route_view_function, methods=[method])
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5001))
-    event_handler = ChangeHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path=DATA_DIR, recursive=False)
-    observer.start()
-
     load_routes()
+    
+    port = int(os.getenv('PORT', 5001))
     app.run(debug=True, port=port, use_reloader=False)
 
-    observer.stop()
-    observer.join()
