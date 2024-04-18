a040a458cff9d74de5c500f277e002cc7f0ab024
Diego Maia <ovrdoz@gmail.com>
2024-04-17 23:38:52 -0300

    refactor code to register methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 import os
 import json
@@ -10,8 +10,6 @@ app = Flask(__name__)
 CORS(app)
 
 DATA_DIR = './data'
-routes = {}
-patterns = {}
 
 class ChangeHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -23,9 +21,6 @@ def convert_route_to_flask(route):
     return re.sub(r'\{([^}]+)\}', r'<\1>', route)
 
 def load_routes():
-    global routes, patterns
-    routes.clear()
-    patterns.clear()
     for filename in os.listdir(DATA_DIR):
         if filename.endswith('.mok'):
             file_path = os.path.join(DATA_DIR, filename)
@@ -36,32 +31,17 @@ def load_routes():
                 http_method = parts[1] if len(parts) > 1 else 'GET'
                 status_code = int(parts[2]) if len(parts) > 2 else 200
 
+                # Convert route variables from {var} to <var>
                 flask_route = convert_route_to_flask(route)
-                flask_route_with_method = f"{flask_route} {http_method.upper()}"
 
                 content = json.load(file)
-                routes[flask_route_with_method] = (content, http_method.upper(), status_code)
+                register_route(flask_route, http_method, content, status_code)
 
-                regex_route = re.sub(r'<([^>]+)>', r'(?P<\1>[^/]+)', flask_route)
-                patterns[regex_route] = flask_route  # Only store flask_route here
-
-
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
-def dynamic_route(path):
-    full_path = '/' + path
-    request_method = request.method
-    for pattern, flask_route in patterns.items():
-        match = re.match(pattern, full_path)
-        if match:
-            flask_route_with_method = f"{flask_route} {request_method}"
-            if flask_route_with_method in routes:
-                content, method, status_code = routes[flask_route_with_method]
-                if request.method == method:
-                    return jsonify(content), status_code
-                else:
-                    return jsonify({"error": "Method Not Allowed"}), 405
-            return jsonify({"error": "Method Not Allowed"}), 405
-    return jsonify({"error": "Route not found"}), 404
+def register_route(route, method, content, status_code):
+    def route_view_function(**kwargs):
+        return jsonify(content), status_code
+    endpoint = f"{route.replace('/', '_')}_{method.lower()}"
+    app.add_url_rule(route, endpoint, route_view_function, methods=[method])
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5001))
@@ -70,7 +50,7 @@ if __name__ == '__main__':
     observer.schedule(event_handler, path=DATA_DIR, recursive=False)
     observer.start()
 
-    load_routes()  
+    load_routes()
     app.run(debug=True, port=port, use_reloader=False)
 
     observer.stop()
